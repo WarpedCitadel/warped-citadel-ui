57ddb12c650a6320f7ec8ef56862929d73f039e6
Jaeger Nolte <jaeger.nolte@gmail.com>
2026-06-27 15:42:55 -0500

    Updated api endpoints to have a reverse proxy using Nginx for local development. Endpoints begin with /api to help cloudfront map urls to backend services without needing to setup environment variables

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,16 +1,16 @@
 import type { SignupRequest, SignupResponse, LoginRequest, LoginResponse } from '../types';
 
-const API_BASE_URL = 'http://localhost:8080';
 
 //signup api request
 export const signupUser = async (data: SignupRequest): Promise<SignupResponse> => {
-  const response = await fetch(`${API_BASE_URL}/auth/signup`, {
+  const response = await fetch("/api/auth/signup", {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       'x-api-version': '1.0',
     },
+
     body: JSON.stringify(data),
   });
 
@@ -24,17 +24,20 @@ export const signupUser = async (data: SignupRequest): Promise<SignupResponse> =
   return result as SignupResponse;
 };
 
+
 //login api request
 export const loginUser = async (data: LoginRequest): Promise<LoginResponse> => {
-  const response = await fetch(`${API_BASE_URL}/auth/login`, {
+  const response = await fetch(`/api/auth/login`, {
     method: 'POST',
     headers: {
        'Content-Type': 'application/json',
        'Accept': 'application/json',
        'x-api-version': '1.0',
       },
+      
     body: JSON.stringify(data),
   });
+
 
   const result = await response.json();
   console.log("Login API Response:", result); //testing
