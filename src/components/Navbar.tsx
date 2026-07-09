bd0c04cf6bfcb4c577da5d10db19525b99eee080
Kaden <therealspydude@gmail.com>
2026-07-09 14:10:33 -0500

    made navbar arrow a more modern chevron with a open animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,23 @@ const Navbar: React.FC = () => {
               className="profile-button"
               onClick={() => setMenuOpen(!menuOpen)}
             >
-              {displayName} ▼
+              <span>{displayName}</span>
+
+              {/* modern drop down arrow */}
+              <svg
+                className={`dropdown-arrow ${menuOpen ? "open" : ""}`}
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <polyline points="6 9 12 15 18 9" />
+              </svg>
             </button>
 
             {menuOpen && (
