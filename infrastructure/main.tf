c4faffef9c1d0bc90d5e9143b0540a13d5660138
Jared Wood <jared.wood002@gmail.com>
2026-08-15 16:32:49 -0500

    git status# This is a combination of 3 commits.
    www.warpedcitadel.com bucket programmatic configuration
    
    adjusted ignored files to ignore .terraform
    
    adjusted gitignore again

---
+++ infrastructure/main.tf
@@ -0,0 +1,51 @@
+resource "aws_s3_bucket" "website-frontend-s3" {
+  bucket              = "www.warpedcitadel.com"
+  bucket_namespace    = "global"
+  force_destroy       = false
+  object_lock_enabled = false
+  policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action    = "s3:GetObject"
+          Effect    = "Allow"
+          Principal = "*"
+          Resource  = "arn:aws:s3:::www.warpedcitadel.com/*"
+          Sid       = "PublicReadOnly"
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+  region        = "us-east-2"
+  request_payer = "BucketOwner"
+  tags          = {}
+  tags_all      = {}
+  grant {
+    id = "fd6924e58bbe969af28ffd63124ed4bb660a5f52eff86e4978b7669eb488b8ac"
+    permissions = [
+      "FULL_CONTROL",
+    ]
+    type = "CanonicalUser"
+  }
+  server_side_encryption_configuration {
+    rule {
+      bucket_key_enabled = true
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  versioning {
+    enabled    = false
+    mfa_delete = false
+  }
+  website {
+    index_document = "index.html"
+  }
+}
+
+import {
+  to = aws_s3_bucket.website-frontend-s3
+  id = "www.warpedcitadel.com"
+}
