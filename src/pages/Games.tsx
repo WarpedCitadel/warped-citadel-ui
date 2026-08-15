20142c54379f3c77ebc2d2f20e685d1d61b07cf3
Kaden <therealspydude@gmail.com>
2026-08-14 20:53:36 -0500

    replaced dropdown triangle with svg chevron

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -463,7 +463,19 @@ const Games: React.FC = () => {
                                     platformDropdownOpen ? "open" : ""
                                     }`}
                                 >
-                                    ▾
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="20"
+                                        height="20"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2.5"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    >
+                                        <polyline points="6 9 12 15 18 9" />
+                                    </svg>
                                 </span>
                                 </button>
 
